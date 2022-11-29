d4d6b1d34f4042c76c141823b4267db6d3d01b61
3Asterism <842384412@qq.com>
2022-11-29 15:59:53 +0800

    akiFlink 更新为1.13版本 学习批流统一

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -22,12 +22,12 @@ public class WordCount {
     public static class myFlatMapper implements FlatMapFunction<String, Tuple2<String,Integer>>{
 
         @Override
-        public void flatMap(String s, Collector<Tuple2<String, Integer>> collector) throws Exception {
+        public void flatMap(String s, Collector<Tuple2<String, Integer>> out) throws Exception {
             //按空格分词
-            String words[] = s.split(" ");
+            String[] words = s.split(" ");
             //遍历
             for(String word : words){
-                collector.collect(new Tuple2<>(word,1));
+                out.collect(new Tuple2<>(word,1));
             }
         }
     }
---
+++ src/main/java/WordCount1_13.java
@@ -0,0 +1,35 @@
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.UnsortedGrouping;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class WordCount1_13 {
+    public static void main(String[] args) throws Exception {
+        //执行环境
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> stringDataSource = env.readTextFile("E:\\demo\\akiFlink\\src\\main\\resources\\hello.txt");
+        //处理逻辑
+        FlatMapOperator<String, Tuple2<String, Long>> wordAndOne = stringDataSource.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            String[] words = line.split(" ");
+            // 将每个单词转换为二元组输出
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG));
+
+        //按照word进行分组
+        wordAndOne.print();
+        System.out.println("------结束1------");
+        UnsortedGrouping<Tuple2<String, Long>> sum1 = wordAndOne.groupBy(0);
+        //分组内进行聚合统计
+        AggregateOperator<Tuple2<String, Long>> sum = sum1.sum(1);
+        sum.print();
+        System.out.println("------结束2------");
+        wordAndOne.groupBy(0).sum(1).print();
+        System.out.println("------结束3------");
+    }
+}
